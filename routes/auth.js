58015475f767d6435c90ef0670cd7c344f8823f0
Ateeksh <ateesood@gmail.com>
2025-10-18 04:54:26 +0000

    auth js skeleton

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
+router.post('/register' , (req , res) => {
+    res.json({message : 'register endpoint working'})
+});
+
+router.post('/login' , (req , res) => {
+    res.json({'message' : 'login working'})
+});
+
+module.exports = router;
