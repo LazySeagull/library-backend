1e5e84b8c4aeea775996ab14e05fb23897a347ba
Ateeksh <ateesood@gmail.com>
2025-10-18 07:13:59 +0000

    app js main

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const { appendFile } = require('fs');
+const router = express.Router();
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('./routes/auth');
+const bookroutes = require('./routes/books');
+const reviewroutes = require('./routes/reviews');
+const transactionroutes = require('.routes/transactions');
+
+app.use('api/auth' , authRoutes);
+app.use('api/books' , bookroutes);
+app.use('api/reviews' , reviewroutes);
+app.use('api/transactions' , transactionroutes);
+
+//test
+
+app.get('/' , (req , res) => {
+    res.send('library backend is running');
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(4000);
